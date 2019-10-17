286c56a536c5d4c4f03ec3f5efeedc369f71da38
Jaden Antoine <jadenjantoine@gmail.com>
2019-10-16 23:01:36 -0400

    Add files via upload

---
+++ Generators.h
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+char upperGenerator(); //generates upper letter characters
+
+char lowerGenerator();//generates lower letter characters
+
+int numberGenerator();//generates numbers as characters
+
+char specialGenerator();//generates special characters
+
+string Password_Generator(); //randomly generates passowords
+
+string Generator_Preferences(); //generates passwords with user input preferences
